4f5db0328f34d858615c0b33fa338254a70298e8
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-29 19:05:49 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ const preinscription = require("./routes/preinscriptionRoutes.js");
 const port = 8080;
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  origin:[https://astcomptabilite.netlify.app],
+  credentials:true
+));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   if (req.method === "OPTIONS") {
