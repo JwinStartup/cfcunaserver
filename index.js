5015f20606fb03849650ef3831207e551dfb7705
Jwin_Technology <66212736+JwinStartup@users.noreply.github.com>
2024-03-29 19:12:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   if (req.method === "OPTIONS") {
