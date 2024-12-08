09d66aedce7793c4f8c7fd07b818c49fcc4b0284
unknown <jeanakoupaul@gmail.com>
2024-12-08 22:56:29 +0000

    backend mise ajout 2

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-require("dotenv").config();
- require("./config.js");
+require("./config.js");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
